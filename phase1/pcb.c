4b62006c9494aa49ab02c30195bb25d3c7643bd0
notALake <79274694+notALake@users.noreply.github.com>
2025-12-17 23:39:21 +0100

    Refactor insertProcQ for improved readability

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -69,9 +69,11 @@ int emptyProcQ(struct list_head* head)
 void insertProcQ(struct list_head* head, pcb_t* p)
 {
   struct list_head* iter;
-  list_for_each(iter, head) {
+  list_for_each(iter, head)
+  {
     pcb_t* item = container_of(iter, pcb_t, p_list);
-    if (item->p_prio < p->p_prio) {
+    if (item->p_prio < p->p_prio)
+    {
         list_add(&p->p_list, iter->prev);
         return;
     }
