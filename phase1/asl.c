5f04579538b422cb4192d77c3070bb5afc2c954f
TeoilMagno <matteomagnani2005@gmail.com>
2026-01-03 02:28:48 +0100

    implementate le prime 2 fuznioni di asl.c

--- phase1/asl.c
+++ phase1/asl.c
@@ -6,11 +6,55 @@ static struct list_head semd_h;
 
 void initASL()
 {
+  INIT_LIST_HEAD(&semdFree_h);
+  INIT_LIST_HEAD(&semd_h);
+
+  for(int c=0;c<MAXPROC;c++)
+  {
+    list_add(&(semd_table[c].s_link), &semdFree_h);
+    semd_table[c].s_key=NULL;
+    INIT_LIST_HEAD(&semd_table[c].s_procq);
+  }
 }
 
 int insertBlocked(int* semAdd, pcb_t* p)
 {
+  struct list_head *iter;
+  int found=0;
+
+  list_for_each(iter, &semd_h)
+  {
+    semd_t *item=container_of(iter, semd_t, s_link);
+    if(*(item->s_key)==*semAdd)
+    {
+      list_add_tail(&p->p_list, &item->s_procq);
+      found=1;
+      return 0;
+    }
+  }
+
+  if(found==0)
+  {
+    if(list_empty(&semdFree_h))
+      return 1;
+    else
+    {
+      struct list_head *entry=semdFree_h.next;
+      list_del(entry);
+
+      semd_t descriptor;
+      descriptor.s_link=*entry;
+      INIT_LIST_HEAD(&descriptor.s_procq);
+      entry=NULL;
+
+      descriptor.s_key=semAdd;
+      list_add(&(descriptor.s_link), &semd_h);
+      list_add_tail(&p->p_list, &descriptor.s_procq);
+      return 0;
+    }
+  }
 }
+
 
 pcb_t* removeBlocked(int* semAdd)
 {
