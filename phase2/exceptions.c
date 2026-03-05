3cb82559757db6ad4c9a1dd28d4c1943042615b6
notALake <79274694+notALake@users.noreply.github.com>
2026-03-05 10:57:30 +0100

    Update exceptions.c

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -2,4 +2,49 @@
 
 void exceptionHandler()
 {
+  unsigned int cause = getCAUSE();
+  unsigned int causeCode = cause && CAUSE_EXCCODE_MASK;
+
+  if(CAUSE_IS_INT(cause))
+  {
+    //interruptHandler();
+  }
+  else
+  {
+    switch(causeCode)
+    {
+      case 8:
+        //SYSCALL
+        break;
+      
+      case 11:
+        //SYSCALL
+        break;
+      
+      case 24:
+        //TLB
+        break;
+      
+      case 25:
+        //TLB
+        break;
+      
+      case 26:
+        //TLB
+        break;
+      
+      case 27:
+        //TLB
+        break;
+      
+      case 28:
+        //TLB
+        break;
+      
+      default:
+        //Program Trap
+        break;
+    }
+  }
+
 }
